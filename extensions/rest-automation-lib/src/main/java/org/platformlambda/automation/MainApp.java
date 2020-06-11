5beef5392d41be9dd0531ce1554c7b9253002152
Eric Law <eric.cw.law@gmail.com>
2020-06-11 09:02:21 -0700

    touch-up

--- extensions/rest-automation-lib/src/main/java/org/platformlambda/automation/MainApp.java
+++ extensions/rest-automation-lib/src/main/java/org/platformlambda/automation/MainApp.java
@@ -27,7 +27,6 @@ import org.platformlambda.automation.servlets.ServiceGateway;
 import org.platformlambda.automation.util.AsyncTimeoutHandler;
 import org.platformlambda.core.annotations.MainApplication;
 import org.platformlambda.core.models.EntryPoint;
-import org.platformlambda.core.models.LambdaFunction;
 import org.platformlambda.core.system.Platform;
 import org.platformlambda.core.system.ServerPersonality;
 import org.platformlambda.core.util.AppConfigReader;
